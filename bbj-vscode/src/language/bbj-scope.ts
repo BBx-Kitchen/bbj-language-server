b4262ed366b5321f67ee7f73d43c160ea08aedad
Miro Spönemann <miro.spoenemann@typefox.io>
2023-03-06 09:34:43 +0100

    Added LICENSE and copyright headers

--- bbj-vscode/src/language/bbj-scope.ts
+++ bbj-vscode/src/language/bbj-scope.ts
@@ -1,3 +1,9 @@
+/******************************************************************************
+ * Copyright 2023 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
 import {
     AstNode, DefaultScopeComputation, DefaultScopeProvider, EMPTY_SCOPE, LangiumDocument, PrecomputedScopes, ReferenceInfo, Scope, stream
 } from 'langium';
--- bbj-vscode/src/language/bbj-validator.ts
+++ bbj-vscode/src/language/bbj-validator.ts
@@ -1,3 +1,9 @@
+/******************************************************************************
+ * Copyright 2023 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
 import { ValidationAcceptor, ValidationChecks } from 'langium';
 import { BBjAstType, Use } from './generated/ast';
 import type { BBjServices } from './bbj-module';
