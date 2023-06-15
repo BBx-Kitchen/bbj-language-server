aa57f3bb3019b85b849942f76c83c9d6ea07644c
Mark Sujew <mark.sujew@typefox.io>
2023-06-15 12:17:24 +0200

    Improve newline handling for method return statement (#13)

--- bbj-vscode/src/language/bbj-token-builder.ts
+++ bbj-vscode/src/language/bbj-token-builder.ts
@@ -5,17 +5,22 @@ export class BBjTokenBuilder extends DefaultTokenBuilder {
 
     override buildTokens(grammar: GrammarAST.Grammar, options?: TokenBuilderOptions | undefined): TokenVocabulary {
         const tokens = super.buildTokens(grammar, options) as TokenType[];
-        const nextTokenIndex = tokens.findIndex(type => type.name === 'NEXT_TOKEN');
-        const nextToken = tokens.splice(nextTokenIndex, 1);
-        tokens.splice(1, 0, ...nextToken);
+        this.spliceToken(tokens, 'NEXT_TOKEN', 1);
+        this.spliceToken(tokens, 'LINE_BREAK', 0);
         return tokens;
+    }
+
+    private spliceToken(tokens: TokenType[], name: string, index: number) {
+        const nextTokenIndex = tokens.findIndex(type => type.name === name);
+        const nextToken = tokens.splice(nextTokenIndex, 1);
+        tokens.splice(index, 0, ...nextToken);
     }
 
     protected override buildKeywordPattern(keyword: GrammarAST.Keyword, caseInsensitive: boolean): TokenPattern {
         if (keyword.value === 'SLTHEN') {
             return /then/i;
         } else if (keyword.value === 'MLTHENFirst') {
-            return /then[ \t]*(\r?\n|rem[ \t][^\n\r]*\r?\n)/i;
+            return /then(?=[ \t]*(\r?\n|rem[ \t][^\n\r]*\r?\n))/i;
         } else {
             return super.buildKeywordPattern(keyword, caseInsensitive);
         }
@@ -26,6 +31,14 @@ export class BBjTokenBuilder extends DefaultTokenBuilder {
             const token: TokenType = {
                 name: terminal.name,
                 PATTERN: this.regexPatternFunction(/(?<=\r?\n[ \t]*)next(?=[ \t]*([_a-zA-Z][\w_]*(!|\$|%)?)?\r?\n)/i),
+                LINE_BREAKS: true
+            };
+            return token;
+        } else if (terminal.name === 'LINE_BREAK') {
+            const token: TokenType = {
+                name: terminal.name,
+                // Add more expceptional tokens here if an explicit line break token is needed
+                PATTERN: this.regexPatternFunction(/(?<=\r?\n[ \t]*methodret)([ \t]*(\r?\n|rem[ \t][^\n\r]*\r?\n))/i),
                 LINE_BREAKS: true
             };
             return token;
