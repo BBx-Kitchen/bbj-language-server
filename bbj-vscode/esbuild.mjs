1d8defd1c855828553851bf6f4c10532c6faae73
Mark Sujew <mark.sujew@typefox.io>
2023-06-21 17:43:24 +0200

    Add bundler and github actions

---
+++ bbj-vscode/esbuild.mjs
@@ -0,0 +1,24 @@
+//@ts-check
+import * as esbuild from 'esbuild';
+
+const watch = process.argv.includes('--watch');
+const minify = process.argv.includes('--minify');
+
+const ctx = await esbuild.context({
+    entryPoints: ['src/extension.ts', 'src/language/main.ts'],
+    outdir: 'out',
+    bundle: true,
+    target: "es6",
+    loader: { '.ts': 'ts' },
+    external: ['vscode'], // the vscode-module is created on-the-fly and must be excluded.
+    platform: 'node', // VSCode extensions run in a node process
+    sourcemap: !minify,
+    minify
+});
+
+if (watch) {
+    await ctx.watch();
+} else {
+    await ctx.rebuild();
+}
+ctx.dispose();
