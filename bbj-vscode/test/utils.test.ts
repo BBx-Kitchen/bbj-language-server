1a5b21f03b7d3268a58005b7cdb8aa0fd15dc0f9
Dennis Huebner <dennis.huebner@gmail.com>
2023-06-26 11:56:33 +0200

    Adjusted test expectation

--- bbj-vscode/test/utils.test.ts
+++ bbj-vscode/test/utils.test.ts
@@ -4,7 +4,7 @@ import { describe, expect, test } from 'vitest';
 import { collectPrefixes, parseSettings } from '../src/language/bbj-ws-manager';
 
 const exampleInput = `
-classpath=~/git/bbj-language-server/examples/lib/com.google.guava_30.1.0.v20221112-0806.jar:~/someOtherPath.jar
+classpath=~/git/bbj-language-server/examples/lib/com.google.guava_30.1.0.v20221112-0806.jar:~/someOtherPath.jar:~/someOtherPath2.jar
 PREFIX="~/BBJ/utils/" "~/BBJ/plugins/" "~/BBJ/utils/reporting/bbjasper/"
 `;
 
@@ -15,7 +15,8 @@ describe('Paths handling tests', () => {
     
     
     test('Parse prefixes paths test', async () => {
-        expect(parsed.classpath).equal('~/git/bbj-language-server/examples/lib/com.google.guava_30.1.0.v20221112-0806.jar:~/someOtherPath.jar'.replace('~', os.homedir()))
+        const expectVal = '~/git/bbj-language-server/examples/lib/com.google.guava_30.1.0.v20221112-0806.jar:~/someOtherPath.jar:~/someOtherPath2.jar'
+        expect(parsed.classpath.join(':')).equal(expectVal.replace(/~/g, os.homedir()))
 
         const paths = parsed.prefixes
         expect(paths.length).equals(3)
