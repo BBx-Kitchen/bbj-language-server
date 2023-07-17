56185facd4887ac5f3adf73819b5fb251fcd4299
Stephan Wald <swald@basis.com>
2023-07-17 15:31:39 +0200

    fix test

--- bbj-vscode/test/utils.test.ts
+++ bbj-vscode/test/utils.test.ts
@@ -1,19 +1,20 @@
 
 import os from 'os';
 import { describe, expect, test } from 'vitest';
-import { collectPrefixes, parseSettings } from '../src/language/bbj-ws-manager';
+import { parseSettings } from '../src/language/bbj-ws-manager';
 
 const exampleInput = `
 classpath=~/git/bbj-language-server/examples/lib/com.google.guava_30.1.0.v20221112-0806.jar:~/someOtherPath.jar:~/someOtherPath2.jar
 PREFIX="~/BBJ/utils/" "~/BBJ/plugins/" "~/BBJ/utils/reporting/bbjasper/"
 `;
+const prefixfromconfigbbx = "/bbx/utils/ /bbx/plugins/";
+const bbjdir = "/bbx/";
 
 describe('Paths handling tests', () => {
 
-    const parsed = parseSettings(exampleInput)
 
-    
-    
+    const parsed = parseSettings(bbjdir,exampleInput,prefixfromconfigbbx)
+
     test('Parse prefixes paths test', async () => {
         const expectVal = '~/git/bbj-language-server/examples/lib/com.google.guava_30.1.0.v20221112-0806.jar:~/someOtherPath.jar:~/someOtherPath2.jar'
         expect(parsed.classpath.join(':')).equal(expectVal.replace(/~/g, os.homedir()))
