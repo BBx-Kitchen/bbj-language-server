db6d670fdfc2786484491a57fd49759d68c1ca69
Markus Rudolph <markus.rudolph@typefox.io>
2024-11-26 16:24:38 +0100

    Fix line breaks around IF, ELSE, FI... (#201)
    * Single line if-else marked as error #149
    ---------
    Co-authored-by: Dennis Huebner <dennis.huebner@gmail.com>

--- bbj-vscode/src/language/bbj-validator.ts
+++ bbj-vscode/src/language/bbj-validator.ts
@@ -5,12 +5,12 @@
  ******************************************************************************/
 
 import { AstNode, AstUtils, CompositeCstNode, CstNode, DiagnosticInfo, GrammarUtils, LeafCstNode, Properties, Reference, RootCstNode, ValidationAcceptor, ValidationChecks, isCompositeCstNode, isLeafCstNode } from 'langium';
+import { dirname, isAbsolute, relative } from 'path';
 import { TextDocument } from 'vscode-languageserver-textdocument';
 import { Range } from 'vscode-languageserver-types';
 import type { BBjServices } from './bbj-module.js';
-import { BBjAstType, Class, CommentStatement, DefFunction, EraseStatement, InitFileStatement, KeyedFileStatement, MethodDecl, OpenStatement, Option, Use, isArrayDeclarationStatement, isBbjClass, isCommentStatement, isCompoundStatement, isElseStatement, isFieldDecl, isForStatement, isIfEndStatement, isIfStatement, isKeywordStatement, isLabelDecl, isLetStatement, isLibMember, isMethodDecl, isOption, isParameterDecl, isStatement, isSwitchStatement } from './generated/ast.js';
+import { BBjAstType, Class, CommentStatement, CompoundStatement, DefFunction, ElseStatement, EraseStatement, IfEndStatement, IfStatement, InitFileStatement, KeyedFileStatement, MethodDecl, OpenStatement, Option, Statement, Use, isArrayDeclarationStatement, isBbjClass, isCommentStatement, isCompoundStatement, isElseStatement, isFieldDecl, isForStatement, isIfEndStatement, isIfStatement, isKeywordStatement, isLabelDecl, isLetStatement, isLibMember, isMethodDecl, isOption, isParameterDecl, isProgram, isSingleStatement, isStatement, isSwitchStatement } from './generated/ast.js';
 import { JavaInteropService } from './java-interop.js';
-import { dirname, isAbsolute, relative } from 'path';
 import { registerClassChecks } from './validations/check-classes.js';
 
 /**
@@ -20,7 +20,7 @@ export function registerValidationChecks(services: BBjServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.BBjValidator;
     const checks: ValidationChecks<BBjAstType> = {
-        AstNode: validator.checkLinebreaks,
+        AstNode: validator.checkLineBreaks,
         Use: validator.checkUsedClassExists,
         OpenStatement: validator.checkOpenStatementOptions,
         InitFileStatement: validator.checkInitFileStatementOptions,
@@ -34,7 +34,16 @@ export function registerValidationChecks(services: BBjServices) {
     registerClassChecks(registry);
 }
 
-type LinebreakMap = [(node: AstNode) => boolean, string[] | boolean, string[] | boolean, string[] | boolean][]
+type LineBreakMask = {
+    before: string[] | boolean;
+    after: string[] | boolean;
+    both: string[] | boolean;
+}
+
+type LineBreakConfig<T extends AstNode> = [
+    (node: AstNode) => node is T,
+    LineBreakMask | ((node: T) => LineBreakMask)
+]
 
 /**
  * Implementation of custom validations.
@@ -42,87 +51,236 @@ type LinebreakMap = [(node: AstNode) => boolean, string[] | boolean, string[] | 
 export class BBjValidator {
 
     protected readonly javaInterop: JavaInteropService;
-    protected readonly linebreakMap: LinebreakMap = [
-        [isFieldDecl,
-            ['FIELD'],
-            true,
-            false],
-        [isMethodDecl,
-            ['METHOD'],
-            false,
-            ['METHODEND']],
-        [isBbjClass,
-            ['CLASS', 'INTERFACE'],
-            false,
-            ['CLASSEND', 'INTERFACEEND']],
-        [isLibMember,
-            false,
-            false,
-            true],
-        [isIfStatement,
-            true,
-            false,
-            false],
-        [isIfEndStatement,
-            false,
-            false,
-            true],
-        [isElseStatement,
-            false,
-            false,
-            true],
-        [this.isStandaloneStatement,
-            false,
-            false,
-            true],
-    ];
 
     constructor(services: BBjServices) {
         this.javaInterop = services.java.JavaInteropService;
     }
 
-    private isSubFolderOf(folder: string, parentFolder: string) {
-        if(parentFolder === folder) {
-            return true;
+    /**
+       * [predicate, before, after, both]
+       * - predicate: function to filter out a node of interest
+       * - before: needs line break before
+       * - after: needs line break after
+       * - both: needs line break before and after
+       */
+    protected readonly lineBreakMap: LineBreakConfig<any>[] = [
+        [isFieldDecl, {
+            before: ['FIELD'],
+            after: true,
+            both: false
+        }],
+        [isMethodDecl, {
+            before: ['METHOD'],
+            after: false,
+            both: ['METHODEND']
+        }],
+        [isBbjClass, {
+            before: ['CLASS', 'INTERFACE'],
+            after: false,
+            both: ['CLASSEND', 'INTERFACEEND']
+        }],
+        [isLibMember, {
+            before: false,
+            after: false,
+            both: true
+        }],
+        this.ifStatementLineBreaks(),
+        this.elseStatementLineBreaks(),
+        this.ifEndStatementLineBreaks(),
+        this.compoundStatementLineBreaks(),
+        [this.isStandaloneStatement, {
+            before: false,
+            after: false,
+            both: true
+        }],
+    ];
+
+    private ifStatementLineBreaks(): LineBreakConfig<IfStatement> {
+        const mask = (node: IfStatement) => {
+            const lineBreaks = { before: true, after: false, both: false };
+            if (isCompoundStatement(node.$container)) {
+                lineBreaks.before = false;
+            }
+            return lineBreaks
         }
-        const relativePath = relative(parentFolder, folder);
-        return relativePath && !relativePath.startsWith('..') && !isAbsolute(relativePath);
+        return [isIfStatement, mask]
     }
 
-    public checkClassReference<N extends AstNode>(accept: ValidationAcceptor, ref: Reference<Class>|undefined, info: DiagnosticInfo<N>): void {
-        if(!ref) {
+    private elseStatementLineBreaks(): LineBreakConfig<ElseStatement> {
+        const mask = (node: ElseStatement) => {
+            const lineBreaks = { before: false, after: false, both: true };
+            let prev = this.previousStatement(node);
+            while (isSingleStatement(prev) && this.isSameLine(prev, node)) {
+                if (isIfStatement(prev)) {
+                    // ELSE: if previous is IF_THEN - same line
+                    lineBreaks.both = false;
+                    break;
+                } else if (isElseStatement(prev) || isIfEndStatement(prev)) {
+                    // other
+                    break;
+                }
+                prev = this.previousStatement(prev);
+            }
+            return lineBreaks
+        }
+        return [isElseStatement, mask]
+    }
+
+    private ifEndStatementLineBreaks(): LineBreakConfig<IfEndStatement> {
+        const mask = (node: IfEndStatement) => {
+            let lineBreaks = { before: false, after: false, both: true };
+            let prev = this.previousStatement(node);
+            while (isSingleStatement(prev) && this.isSameLine(prev, node)) {
+                if (isIfStatement(prev) || isElseStatement(prev)) {
+                    // ENDIF: if previous is IF_THEN or ELSE same line
+                    lineBreaks.both = false;
+                    break;
+                } else if (isIfEndStatement(prev)) {
+                    // other
+                    break;
+                }
+                prev = this.previousStatement(prev);
+            }
+            return lineBreaks
+        }
+        return [isIfEndStatement, mask]
+    }
+
+    private compoundStatementLineBreaks(): LineBreakConfig<CompoundStatement> {
+        const mask = (node: CompoundStatement) => {
+            const lineBreaks = { before: false, after: false, both: false };
+            if (this.isStandaloneStatement(node)) {
+                // default case - wrap by line breaks
+                lineBreaks.both = true;
+                if (isIfStatement(node.statements[node.statements.length - 1])) {
+                    // case: PRINT "FOO"; IF value = 1 THEN PRINT "BAR" FI
+                    lineBreaks.both = false;
+                    lineBreaks.before = true;
+                    lineBreaks.after = false;
+                }
+            }
+            return lineBreaks
+        }
+        return [isCompoundStatement, mask]
+    }
+
+    private isSameLine(node: AstNode, other: AstNode): boolean {
+        return node.$cstNode?.range.start.line === other.$cstNode?.range.start.line;
+    }
+
+    checkLineBreaks(node: AstNode, accept: ValidationAcceptor): void {
+        const document = AstUtils.getDocument(node);
+        if (document.parseResult.parserErrors.length > 0) {
+            return;
+        }
+        const textDocument = document.textDocument;
+        for (const config of this.lineBreakMap) {
+            const predicate = config[0];
+            if (node.$cstNode && predicate.call(this, node)) {
+                const mask = (config[1] instanceof Function) ? config[1](node) : config[1];
+                if (mask.before) {
+                    const beforeNodes = this.getCstNodes(node.$cstNode, mask.before);
+                    for (const cst of beforeNodes) {
+                        if (!this.hasLinebreakBefore(cst, textDocument)) {
+                            accept('error', 'This statement needs to start in a new line: ' + textDocument.getText(cst.range), {
+                                node,
+                                range: cst.range
+                            });
+                        }
+                    }
+                }
+                if (mask.after) {
+                    const afterNodes = this.getCstNodes(node.$cstNode, mask.after);
+                    for (const cst of afterNodes) {
+                        if (!this.hasLinebreakAfter(cst, textDocument)) {
+                            accept('error', 'This statement needs to end with a line break: ' + textDocument.getText(cst.range), {
+                                node,
+                                range: cst.range
+                            });
+                        }
+                    }
+                }
+                if (mask.both) {
+                    const cstNodes = this.getCstNodes(node.$cstNode, mask.both);
+                    for (const cst of cstNodes) {
+                        let missingMsg: string | undefined;
+                        if (!this.hasLinebreakBefore(cst, textDocument)) {
+                            missingMsg = 'This statement needs to start in a new line';
+                        } else if (!this.hasLinebreakAfter(cst, textDocument)) {
+                            missingMsg = 'This statement needs to end with a line break';
+                        }
+                        if (missingMsg) {
+                            accept('error', `${missingMsg}: ${textDocument.getText(cst.range)}`, {
+                                node,
+                                range: cst.range
+                            });
+                        }
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    private previousStatement(statement: Statement): Statement | undefined {
+        const container = statement.$container;
+        if (isProgram(container) || isCompoundStatement(container)) {
+            if (statement.$containerIndex === 0 && isCompoundStatement(container)) {
+                return this.previousStatement(container);
+            } else if (statement.$containerIndex && statement.$containerIndex > 0) {
+                const prevSibling = container.statements[statement.$containerIndex - 1];
+                if (isCompoundStatement(prevSibling)) {
+                    // last child statement in compound statement
+                    return prevSibling.statements[prevSibling.statements.length - 1];
+                } else if (isStatement(prevSibling)) {
+                    return prevSibling;
+                }
+            }
+        }
+        return undefined;
+    }
+
+    public checkClassReference<N extends AstNode>(accept: ValidationAcceptor, ref: Reference<Class> | undefined, info: DiagnosticInfo<N>): void {
+        if (!ref) {
             return;
         }
         const uriOfUsage = AstUtils.getDocument(ref.$refNode!.root.astNode).uri.fsPath;
-        if(!ref.ref) {
+        if (!ref.ref) {
             return;
         }
         const klass = ref.ref;
-        if(isBbjClass(klass) && klass.visibility) {
+        if (isBbjClass(klass) && klass.visibility) {
             const typeName = klass.interface ? 'interface' : 'class';
             const uriOfDeclaration = AstUtils.getDocument(ref.ref).uri.fsPath;
-            switch(klass.visibility.toUpperCase()) {
+            switch (klass.visibility.toUpperCase()) {
                 case "PUBLIC":
                     //everything is allowed
                     return;
                 case "PROTECTED":
                     const dirOfDeclaration = dirname(uriOfDeclaration);
                     const dirOfUsage = dirname(uriOfUsage);
-                    if(!this.isSubFolderOf(dirOfUsage, dirOfDeclaration)) {
+                    if (!this.isSubFolderOf(dirOfUsage, dirOfDeclaration)) {
                         accept("error", `Protected ${typeName} '${klass.name}' can be only referenced within the same directory!`, info);
                     }
                     break;
                 case "PRIVATE":
-                    if(uriOfUsage !== uriOfDeclaration) {
+                    if (uriOfUsage !== uriOfDeclaration) {
                         accept("error", `Private ${typeName} '${klass.name}' can be only referenced within the same file!`, info);
                     }
                     break;
             }
         }
     }
-    
 
-    private isStandaloneStatement(node: AstNode): boolean {
+    private isSubFolderOf(folder: string, parentFolder: string) {
+        if (parentFolder === folder) {
+            return true;
+        }
+        const relativePath = relative(parentFolder, folder);
+        return relativePath && !relativePath.startsWith('..') && !isAbsolute(relativePath);
+    }
+
+    private isStandaloneStatement(node: AstNode): node is Statement {
         const previous = this.getPreviousNode(node);
         if (isLabelDecl(node) || isLabelDecl(previous)) {
             return false;
@@ -133,10 +291,22 @@ export class BBjValidator {
                 || isForStatement(node.$container)
                 || isArrayDeclarationStatement(node.$container)
                 || AstUtils.getContainerOfType(previous, isSwitchStatement)
-                || AstUtils.getContainerOfType(previous, isIfStatement)) {
+                || AstUtils.getContainerOfType(previous, isIfStatement)
+                || this.isInsideSingleLineIf(node)) {
                 return false;
             }
             return true;
+        }
+        return false;
+    }
+
+    private isInsideSingleLineIf(node: Statement): boolean {
+        let prev = this.previousStatement(node);
+        while (prev && this.isSameLine(prev, node)) {
+            if (isIfStatement(prev)) {
+                return true;
+            }
+            prev = this.previousStatement(prev);
         }
         return false;
     }
@@ -152,7 +322,7 @@ export class BBjValidator {
 
     checkIfMethodIsChildOfInterface(node: MethodDecl, accept: ValidationAcceptor): void {
         const klass = AstUtils.getContainerOfType(node, isBbjClass)!;
-        if(klass.interface && node.endTag) {
+        if (klass.interface && node.endTag) {
             accept('error', 'Methods of interfaces must not have a METHODEND keyword!', {
                 node: node,
                 property: 'endTag'
@@ -182,51 +352,7 @@ export class BBjValidator {
         }
     }
 
-    checkLinebreaks(node: AstNode, accept: ValidationAcceptor): void {
-        const document = AstUtils.getDocument(node);
-        if (document.parseResult.parserErrors.length > 0) {
-            return;
-        }
-        const textDocument = document.textDocument;
-        for (const [predicate, before, after, both] of this.linebreakMap) {
-            if (node.$cstNode && predicate.call(this, node)) {
-                if (before) {
-                    const beforeNodes = this.getCstNodes(node.$cstNode, before);
-                    for (const cst of beforeNodes) {
-                        if (!this.hasLinebreakBefore(cst, textDocument)) {
-                            accept('error', 'This line needs to be preceeded by a line break: ' +textDocument.getText(cst.range), {
-                                node,
-                                range: cst.range
-                            });
-                        }
-                    }
-                }
-                if (after) {
-                    const afterNodes = this.getCstNodes(node.$cstNode, after);
-                    for (const cst of afterNodes) {
-                        if (!this.hasLinebreakAfter(cst, textDocument)) {
-                            accept('error', 'This line needs to be succeeded by a line break: ' +textDocument.getText(cst.range), {
-                                node,
-                                range: cst.range
-                            });
-                        }
-                    }
-                }
-                if (both) {
-                    const cstNodes = this.getCstNodes(node.$cstNode, both);
-                    for (const cst of cstNodes) {
-                        if (!this.hasLinebreakAfter(cst, textDocument) || !this.hasLinebreakBefore(cst, textDocument)) {
-                            accept('error', 'This line needs to be wrapped by line breaks: ' +textDocument.getText(cst.range) , {
-                                node,
-                                range: cst.range
-                            });
-                        }
-                    }
-                }
-                break;
-            }
-        }
-    }
+
 
     private getCstNodes(node: CstNode, features: string[] | boolean): CstNode[] {
         if (Array.isArray(features)) {
@@ -369,6 +495,8 @@ function binarySearch(node: CompositeCstNode, offset: number): CstNode | undefin
         }
     }
 
+
     return closest;
 }
+
 
