5fb8f2052fe56aca32233dc02b6f98a5868616e2
Markus Rudolph <markus.rudolph@typefox.io>
2025-02-20 16:24:40 +0100

    Fix tests for implements and extends when index is 0

--- bbj-vscode/src/language/bbj-scope.ts
+++ bbj-vscode/src/language/bbj-scope.ts
@@ -60,9 +60,9 @@ export class BbjScopeProvider extends DefaultScopeProvider {
 
     override getScope(context: ReferenceInfo): Scope {
         if(isBbjClass(context.container)) {
-            if(context.property === 'extends' && context.container.extends && context.index) {
+            if(context.property === 'extends' && context.container.extends && typeof context.index === 'number') {
                 return this.resolveClassScopeByName(context, context.container.extends[context.index].$refText);
-            } else if (context.property === 'implements' && context.container.implements && context.index) {
+            } else if (context.property === 'implements' && context.container.implements && typeof context.index === 'number') {
                 return this.resolveClassScopeByName(context, context.container.implements[context.index].$refText);
             }
         } else if(isVariableDecl(context.container) && context.container.type) {
