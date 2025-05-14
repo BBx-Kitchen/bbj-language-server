058f26261abc4734421af51acef2c07af5e65881
Christopher Kochaneck <ckochaneck@basis.cloud>
2025-05-14 14:21:53 +0200

    feat: Implemented logic to denumber

--- bbj-vscode/src/Commands/Commands.cjs
+++ bbj-vscode/src/Commands/Commands.cjs
@@ -49,6 +49,47 @@ const runWeb = (params, client) => {
   });
 };
 
+const decompile = (params, options = {}) => {
+  const home = getBBjHome();
+  if (!home) return;  
+  
+  const active = vscode.window.activeTextEditor;
+  const fileName = active ? active.document.fileName : params.fsPath;
+  const resolvedFileName = path.resolve(fileName);
+  const resolvedLstFileName = resolvedFileName + '.lst';
+  
+  const newFileName = options.denumber ? resolvedFileName : resolvedFileName.replace(".lst", "");
+  
+  const flags = options.denumber ? ' -l ' : '';
+  const cmd = `"${home}/bin/bbjlst${os.platform() === 'win32' ? '.exe' : ''}" ${flags} "${resolvedFileName}"`;
+
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      console.log(err.message);
+      vscode.window.showErrorMessage(`Failed to decompile "${fileName}"`);
+      return;
+    }
+
+    fs.unlink(resolvedFileName, (unlinkErr) => {
+      if (unlinkErr) {
+        vscode.window.showErrorMessage(`Failed to remove original file "${fileName}": ${unlinkErr.message}`);
+        return;
+      }      
+      
+      fs.rename(resolvedLstFileName, newFileName, (renameErr) => {
+        if (renameErr) {
+          vscode.window.showErrorMessage(`Failed to rename decompiled file "${fileName}": ${renameErr.message}`);
+          return;
+        }
+
+        const uri = vscode.Uri.file(newFileName);
+        vscode.workspace.openTextDocument(uri).then((doc) => {
+          vscode.window.showTextDocument(doc, { preview: false });
+        });
+      });
+    });
+  });
+};
 
 const Commands = {
   openConfigFile: function () {
@@ -127,42 +168,28 @@ const Commands = {
     runWeb(params, "DWC");
   },
 
-  denumber: function(params) {
+  compile: function (params) {
     const home = getBBjHome();
     if (!home) return;
+    const cmd = `"${home}/bin/bbjcpl${os.platform() === "win32" ? ".exe" : ""}" "${vscode.window.activeTextEditor.document.fileName}"`;
 
     const active = vscode.window.activeTextEditor;
-    const fileName = active ? active.document.fileName : params.fsPath;
-    const resolvedFileName = path.resolve(fileName);
-    const resolvedLstFileName = path.resolve(fileName) + ".lst";
 
-    const cmd = `"${home}/bin/bbjlst${os.platform() === 'win32' ? '.exe' : ''}" "${resolvedFileName}" -l`;
-
+    const fileName = active ? active.document.fileName : params.fsPath;;
     exec(cmd, (err, stdout, stderr) => {
       if (err) {
-        vscode.window.showErrorMessage(`Failed to decompile "${fileName}"`);
+        vscode.window.showErrorMessage(`Failed to compile "${fileName}"`);
         return;
       }
-
-      fs.unlink(resolvedFileName, (unlinkErr) => {
-        if (unlinkErr) {
-          vscode.window.showErrorMessage(`Failed to remove original file "${fileName}": ${unlinkErr.message}`);
-          return;
-        }
-
-        fs.rename(resolvedLstFileName, resolvedFileName, (renameErr) => {
-          if (renameErr) {
-            vscode.window.showErrorMessage(`Failed to rename decompiled file "${fileName}": ${renameErr.message}`);
-            return;
-          }
-
-          const uri = vscode.Uri.file(resolvedFileName);
-          vscode.workspace.openTextDocument(uri).then((doc) => {
-            vscode.window.showTextDocument(doc, { preview: false });
-          });
-        });
-      });
     });
+    vscode.window.showInformationMessage(`Successfully compiled "${fileName}"`);
+  },
+  decompile: function (params) {
+    decompile(params);
+  },
+
+  denumber: function(params) {
+    decompile(params, { denumber: true });
   }
 };
 
