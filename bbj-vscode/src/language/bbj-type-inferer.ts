54c81a7f264c9bb2d8fb12a754aed8773387dcf6
Stephan Wald <swald@basis.com>
2026-02-06 10:01:47 +0100

    feat(25-01): add CAST() and implicit getter type resolution
    - Add CAST() type extraction from first argument (BBjTypeRef or Class SymbolRef)
    - Add FieldDecl handling in isMemberCall branch for implicit getter type conveyance
    - Import isLibFunction type guard
    - Return undefined for unresolvable CAST types (treat as untyped)

--- bbj-vscode/src/language/bbj-type-inferer.ts
+++ bbj-vscode/src/language/bbj-type-inferer.ts
@@ -1,6 +1,6 @@
 import { BBjServices } from "./bbj-module.js";
 import { getClass } from "./bbj-nodedescription-provider.js";
-import { Assignment, Class, Expression, isArrayDecl, isAssignment, isBBjTypeRef, isClass, isConstructorCall, isFieldDecl, isJavaField, isJavaMethod, isJavaPackage, isMemberCall, isMethodCall, isMethodDecl, isStringLiteral, isSymbolRef, isVariableDecl, JavaPackage } from "./generated/ast.js";
+import { Assignment, Class, Expression, isArrayDecl, isAssignment, isBBjTypeRef, isClass, isConstructorCall, isFieldDecl, isJavaField, isJavaMethod, isJavaPackage, isLibFunction, isMemberCall, isMethodCall, isMethodDecl, isStringLiteral, isSymbolRef, isVariableDecl, JavaPackage } from "./generated/ast.js";
 import { JavaInteropService } from "./java-interop.js";
 
 export interface TypeInferer {
@@ -40,6 +40,8 @@ export class BBjTypeInferer implements TypeInferer {
                     return member.resolvedReturnType?.ref;
                 } else if (isMethodDecl(member)) {
                     return getClass(member.returnType);
+                } else if (isFieldDecl(member)) {
+                    return getClass(member.type);
                 } else if (isJavaPackage(member) || isClass(member)) {
                     return member;
                 }
@@ -49,6 +51,26 @@ export class BBjTypeInferer implements TypeInferer {
         } else if (isStringLiteral(expression)) {
             return this.javaInterop.getResolvedClass('java.lang.String')
         } else if(isMethodCall(expression)) {
+            // Check if this is a CAST() call
+            if (isSymbolRef(expression.method)) {
+                const methodRef = expression.method.symbol.ref;
+                if (isLibFunction(methodRef) && methodRef.name.toLowerCase() === 'cast') {
+                    // CAST(type, object) - first argument is the type
+                    if (expression.args.length > 0) {
+                        const typeArg = expression.args[0].expression;
+                        if (isBBjTypeRef(typeArg)) {
+                            return typeArg.klass.ref;
+                        } else if (isSymbolRef(typeArg)) {
+                            const typeRef = typeArg.symbol.ref;
+                            if (isClass(typeRef)) {
+                                return typeRef;
+                            }
+                        }
+                    }
+                    // If type is unresolvable, return undefined (treat as untyped)
+                    return undefined;
+                }
+            }
             return this.getType(expression.method);
         } else if(isBBjTypeRef(expression)) {
             return expression.klass.ref;
